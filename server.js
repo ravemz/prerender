0b31e162a741cdc4c799395574cbf4b53f42fcc0
Gaurav Sahni <gauravsahni@gmail.com>
2023-12-04 15:22:01 +0530

    some fixes copied from active PRs on prerender

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+	// pageDoneCheckInterval: 5000,
+	// waitAfterLastRequest: 5000,
+	// parseShadowDom: true,
+	// logRequests: true
+});
 
-server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
 server.use(prerender.addMetaTags());
+server.use(prerender.updateStyleTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
