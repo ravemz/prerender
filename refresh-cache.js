9fb58e16912fc40d3d6b312b2010027543863662
Gaurav Sahni <gauravsahni@gmail.com>
2023-12-14 18:00:05 +0530

    added error checks and TTL to not expire

--- refresh-cache.js
+++ refresh-cache.js
@@ -13,7 +13,7 @@ if (!prerender_url) {
 const args = arg_parser(process.argv);
 const {
     ["sitemap-url"]: sitemap_url,
-    ["api-rate-limit"]: api_rate_limit = 500,
+    ["api-rate-limit"]: api_rate_limit = 10,
     ["1"]: single_url = undefined,
 } = args;
 
@@ -30,16 +30,15 @@ async function refreshCacheForSitemap(sitemap_url) {
     const Sitemapper = require("sitemapper");
     const sitemap = new Sitemapper();
 
-    console.log("fetching sitemap from", sitemap_url);
+    console.log("fetching sitemap from: ", sitemap_url);
     const { sites, errors } = await sitemap.fetch(sitemap_url);
-    console.error("faced errors:", errors);
+    console.error("faced errors: ", errors);
 
-    console.log(
-        "refreshing cache for",
-        sites.length,
-        "urls from sitemap located at",
-        sitemap_url
-    );
+    refreshCacheFromArray(sites);
+}
+
+async function refreshCacheFromArray(sites) {
+    console.log("refreshing cache for: ", sites.length, " urls");
 
     for (let i = 0; i < sites.length / api_rate_limit; i++) {
         await Promise.all(
@@ -48,15 +47,19 @@ async function refreshCacheForSitemap(sitemap_url) {
                 .map(refreshCacheForUrl)
         );
         // wait for 1min after api_rate_limit calls
-        await new Promise((resolve) => setTimeout(resolve, 1000 * 60));
+        // await new Promise((resolve) => setTimeout(resolve, 1000 * 60));
     }
 }
 
 async function refreshCacheForUrl(url) {
-    console.log("refreshing cache for", url);
-    return await axios({
-        url: prerender_url + "/render",
-        method: "post",
-        data: { url },
-    });
+    console.log("refreshing cache for: ", url);
+    try {
+      return await axios({
+          url: prerender_url + "/render",
+          method: "post",
+          data: { url },
+      });
+    } catch (err) {
+      console.error("error for url: "+url, err.message);
+    }
 }
